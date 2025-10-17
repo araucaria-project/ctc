ba4ee05e831ba17299d59b9390953366f4b10af5
Mmiirrkk <cot@wp.pl>
2025-10-17 14:10:09 +0200

    add test 1

--- test/test_data_clean.py
+++ test/test_data_clean.py
@@ -4,6 +4,26 @@ from ctc.cycle_time_data_clean import CycleTimeDataClean
 
 
 
-asyncio.run(CycleTimeDataClean.data_clean_all(
-    base_folder='/home/mirk/Desktop/misc/cycle_time_calc/old1/'
-))
+# asyncio.run(CycleTimeDataClean.data_clean_all(
+#     base_folder='/home/mirk/Desktop/misc/cycle_time_calc/old1/'
+# ))
+
+
+l = []
+with open('/home/mirk/Desktop/misc/cycle_time_calc/old2/wk06_OBJECT_clean_data.txt') as f:
+    f = f.read()
+
+for n in f.split('\n'):
+    if n != '':
+        l.append(n)
+
+d = []
+with open('/home/mirk/Desktop/misc/cycle_time_calc/old1/wk06_OBJECT_clean_data.txt') as f:
+    f = f.read()
+
+for n in f.split('\n'):
+    if n != '':
+        if n not in l:
+            d.append(n)
+
+print(d)
