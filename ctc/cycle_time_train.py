a055f6dcd4fd198f561f8f99a30705576801e83b
Mmiirrkk <cot@wp.pl>
2026-01-23 13:50:39 +0100

    change to last 2000 records 1

--- ctc/cycle_time_train.py
+++ ctc/cycle_time_train.py
@@ -19,7 +19,7 @@ class CycleTimeTrain(AbstractCycleTime):
     """
 
     _MIN_DATA_RECORDS_TO_TRAIN = 10
-    _MAX_DATA_RECORDS_TO_TRAIN = 1500
+    _MAX_DATA_RECORDS_TO_TRAIN = 2000
 
     @staticmethod
     async def _build_array(data: List[Dict[str, Any]], xory: str) -> Optional[np.ndarray]:
