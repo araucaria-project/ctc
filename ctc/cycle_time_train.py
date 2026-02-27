22260a053cf34b9ec07dc24ae325c973932243f7
Mmiirrkk <cot@wp.pl>
2026-02-27 14:48:45 +0100

    add 3500 rec 1a

--- ctc/cycle_time_train.py
+++ ctc/cycle_time_train.py
@@ -19,7 +19,7 @@ class CycleTimeTrain(AbstractCycleTime):
     """
 
     _MIN_DATA_RECORDS_TO_TRAIN = 10
-    _MAX_DATA_RECORDS_TO_TRAIN = 3500
+    _MAX_DATA_RECORDS_TO_TRAIN = 5000
 
     @staticmethod
     async def _build_array(data: List[Dict[str, Any]], xory: str) -> Optional[np.ndarray]:
