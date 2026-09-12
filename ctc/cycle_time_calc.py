501c552e161c587de70b7414cf2627c8df0d1119
MMiirrkk <cot@wp.pl>
2026-09-12 21:05:37 +0200

    fix ut wait 1c

--- ctc/cycle_time_calc.py
+++ ctc/cycle_time_calc.py
@@ -333,14 +333,12 @@ class CycleTimeCalc(AbstractCycleTime):
                         second=int(ut_str.split(':')[2]),
                         microsecond=0
                     )
-                    if _start > _end:
+                    if _start + datetime.timedelta(hours=12) > _end:
+                        return 0
+                    else:
                         _end = _end + datetime.timedelta(days=1)
-                    if _start > _end:
-                        _end = _end + datetime.timedelta(days=1)
-                    if _start > _end:
-                        _end = _end + datetime.timedelta(days=1)
+                        return (_end - _start).total_seconds()
 
-                    return (_end - _start).total_seconds()
 
                 elif 'sunrise' in command_dict['kwargs'].keys():
                     now = self._start_time + datetime.timedelta(seconds=self._time_length)
